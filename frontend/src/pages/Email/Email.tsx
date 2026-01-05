48df20d236b2698b01bd908673ee4a33c9cf5cf2
gyawat.magar <Gyawat.magar@gmail.com>
2026-01-05 14:47:23 +0200

    add email page

---
+++ frontend/src/pages/Email/Email.tsx
@@ -0,0 +1,11 @@
+import react from 'react';
+import './Email.css';
+
+
+export const Email: React.FC = () => {
+  return (
+    <div className="email-page">
+        <h1>Email Page</h1>
+    </div>
+  );
+};
